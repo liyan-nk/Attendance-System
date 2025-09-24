a270db337c4f3a6de2282577ad4efa071e19f4a5
Liyan Nechikaden <lnk.liyannk@gmail.com>
2025-09-24 21:24:57 +0530

    Deletion

--- SecureAttendancePhase2/backend/models/student.py
+++
@@ -1,8 +0,0 @@
-from database import db
-
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    roll_no = db.Column(db.String(20), unique=True, nullable=False)
-    name = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    device_token = db.Column(db.String(200), unique=True)  # optional: one device per student
